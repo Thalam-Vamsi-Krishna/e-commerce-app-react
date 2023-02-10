de6873d48e2c6aa6ffe9c14313f7ed47b0a5369f
Thalam-Vamsi-Krishna <vamsikrishna.thalam@gmail.com>
2023-02-10 11:00:42 +0530

    successfully implemented

---
+++ src/components/Layout/Body.js
@@ -0,0 +1,113 @@
+import { productsArr } from "../../data/productData";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+const Body = () => {
+  return (
+    <div>
+      <h2 style={{ textAlign: "center" }}>Music</h2>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <div>
+          {productsArr.map((product, index) => {
+            if (index % 2 === 0) {
+              return (
+                <Card
+                  style={{
+                    width: "18rem",
+                    margin: "20px",
+                    marginRight: "70px",
+                    border: "none",
+                  }}
+                  key={index}
+                >
+                  <Card.Body>
+                    <Card.Title
+                      style={{ marginBottom: "15px", textAlign: "center" }}
+                    >
+                      {product.title}
+                    </Card.Title>
+                    <Card.Img
+                      variant="top"
+                      src={product.imageUrl}
+                      style={{
+                        transition: "transform .3s",
+                        width: "100%",
+                        height: "100%",
+                      }}
+                      onMouseOver={(e) => {
+                        e.currentTarget.style.transform = "scale(1.1)";
+                      }}
+                      onMouseOut={(e) => {
+                        e.currentTarget.style.transform = "scale(1)";
+                      }}
+                    />
+                    <Card.Text style={{ float: "left", marginTop: "15px" }}>
+                      ${product.price}
+                    </Card.Text>
+                    <Button
+                      variant="primary"
+                      style={{ float: "right", marginTop: "15px" }}
+                    >
+                      Add to Cart
+                    </Button>
+                    <div style={{ clear: "both" }} />
+                  </Card.Body>
+                </Card>
+              );
+            }
+          })}
+        </div>
+        <div>
+          {productsArr.map((product, index) => {
+            if (index % 2 !== 0) {
+              return (
+                <Card
+                  style={{
+                    width: "18rem",
+                    margin: "20px",
+                    marginLeft: "70px",
+                    border: "none",
+                  }}
+                  key={index}
+                >
+                  <Card.Body>
+                    <Card.Title
+                      style={{ marginBottom: "15px", textAlign: "center" }}
+                    >
+                      {product.title}
+                    </Card.Title>
+                    <Card.Img
+                      variant="top"
+                      src={product.imageUrl}
+                      style={{
+                        transition: "transform .3s",
+                        width: "100%",
+                        height: "100%",
+                      }}
+                      onMouseOver={(e) => {
+                        e.currentTarget.style.transform = "scale(1.1)";
+                      }}
+                      onMouseOut={(e) => {
+                        e.currentTarget.style.transform = "scale(1)";
+                      }}
+                    />
+                    <Card.Text style={{ float: "left", marginTop: "15px" }}>
+                      ${product.price}
+                    </Card.Text>
+                    <Button
+                      variant="primary"
+                      style={{ float: "right", marginTop: "15px" }}
+                    >
+                      Add to Cart
+                    </Button>
+                    <div style={{ clear: "both" }} />
+                  </Card.Body>
+                </Card>
+              );
+            }
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Body;
